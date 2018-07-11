7249905b8f47117d6fb8751ba788dc68c2f00f76
netflixandbill <31534191+netflixandbill@users.noreply.github.com>
2018-07-11 10:16:31 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,122 @@
+const json5 = require('json5');
+const fs = require('fs');
+const path = require('path');
+
+let userConfig;
+
+// Try to find our config file from several options
+const configFiles = [
+  'config.json',
+  'config.json5',
+  'config.json.json',
+  'config.json.txt'
+];
+
+let foundConfigFile;
+
+for (const configFile of configFiles) {
+  try {
+    fs.accessSync(__dirname + '/../' + configFile);
+    foundConfigFile = configFile;
+    break;
+  } catch (e) {}
+}
+
+if (! foundConfigFile) {
+  throw new Error(`Could not find config.json!`);
+}
+
+// Parse the config using JSON5
+try {
+  const raw = fs.readFileSync(__dirname + '/../' + foundConfigFile);
+  userConfig = json5.parse(raw);
+} catch (e) {
+  throw new Error(`Error reading config file! The error given was: ${e.message}`);
+}
+
+const defaultConfig = {
+  "token": null,
+  "mailGuildId": null,
+  "mainGuildId": null,
+  "logChannelId": null,
+
+  "prefix": "!",
+  "snippetPrefix": "!!",
+
+  "status": "Message me for help!",
+  "responseMessage": "Thank you for your message! Our mod team will reply to you here as soon as possible.",
+  "closeMessage": null,
+
+  "newThreadCategoryId": null,
+  "mentionRole": "here",
+  "pingOnBotMention": true,
+
+  "inboxServerPermission": null,
+  "alwaysReply": false,
+  "alwaysReplyAnon": false,
+  "useNicknames": false,
+  "ignoreAccidentalThreads": false,
+  "threadTimestamps": false,
+  "allowMove": false,
+  "typingProxy": false,
+  "typingProxyReverse": false,
+
+  "enableGreeting": false,
+  "greetingMessage": null,
+  "greetingAttachment": null,
+
+  "relaySmallAttachmentsAsAttachments": false,
+
+  "port": 8890,
+  "url": null,
+
+  "dbDir": path.join(__dirname, '..', 'db'),
+  "knex": null,
+
+  "logDir": path.join(__dirname, '..', 'logs'),
+};
+
+const required = ['token', 'mailGuildId', 'mainGuildId', 'logChannelId'];
+
+const finalConfig = Object.assign({}, defaultConfig);
+
+for (const [prop, value] of Object.entries(userConfig)) {
+  if (! defaultConfig.hasOwnProperty(prop)) {
+    throw new Error(`Invalid option: ${prop}`);
+  }
+
+  finalConfig[prop] = value;
+}
+
+// Default knex config
+if (! finalConfig['knex']) {
+  finalConfig['knex'] = {
+    client: 'sqlite',
+      connection: {
+      filename: path.join(finalConfig.dbDir, 'data.sqlite')
+    },
+    useNullAsDefault: true
+  };
+}
+
+// Make sure migration settings are always present in knex config
+Object.assign(finalConfig['knex'], {
+  migrations: {
+    directory: path.join(finalConfig.dbDir, 'migrations')
+  }
+});
+
+// Make sure all of the required config options are present
+for (const opt of required) {
+  if (! finalConfig[opt]) {
+    console.error(`Missing required config.json value: ${opt}`);
+    process.exit(1);
+  }
+}
+
+// Make sure mainGuildId is internally always an array
+if (! Array.isArray(finalConfig['mainGuildId'])) {
+  finalConfig['mainGuildId'] = [finalConfig['mainGuildId']];
+}
+
+module.exports = finalConfig;
